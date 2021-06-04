ada01a59bd520d5609ee18dce763414d4fd76084
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-03 20:38:18 -0500

    added increment and decrement functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from './redux/counter';
 function App() {
-  const [count, setCount ] = React.useState(0);
+  const {count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Count: {count}</h1>
-      <button onClick={()=> setCount(count +1)}>+</button>
-      <button onClick={()=> setCount(count-1)}>-</button>
+      <button onClick={()=> dispatch(increment())}>+</button>
+      <button onClick={()=> dispatch(decrement())}>-</button>
     </div>
   );
 }
